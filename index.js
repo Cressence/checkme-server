f74ae029ce11c23e5eb811fe53a2179124f39d6a
Cressence <cressencey@gmail.com>
2020-04-09 00:46:39 +0100

    Serve content depending on lng parameter

--- index.js
+++ index.js
@@ -4,22 +4,37 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const home_en = require('./src/home_en.json');
+const home_fr = require('./src/home_fr.json');
 const help_en = require('./src/help_en.json');
+const help_fr = require('./src/help_fr.json');
 const statistics_en = require('./src/statistics_en.json');
+const statistics_fr = require('./src/statistics_fr.json');
 
 // serve home screen data
 app.get("/home", (req, res) => {
-    res.send(home_en);
+    if (req.query.lng == "fr") {
+        res.send(home_fr);
+    } else {
+        res.send(home_en);
+    }
 });
 
 // serve help screen data
 app.get("/help", (req, res) => {
-    res.send(help_en);
+    if (req.query.lng == "fr") {
+        res.send(help_fr);
+    } else {
+        res.send(help_en);
+    }
 });
 
 // serve statistics data
 app.get("/statistics", (req, res) => {
-    res.send(statistics_en);
+    if (req.query.lng == "fr") {
+        res.send(statistics_fr);
+    } else {
+        res.send(statistics_fr);
+    }
 });
 
 
